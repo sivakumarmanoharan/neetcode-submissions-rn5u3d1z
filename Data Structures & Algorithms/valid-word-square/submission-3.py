8e13489265f7a9d0fcb1fdc3f998b46af6358c08
Sivakumar Manoharan <42244366+sivakumarmanoharan@users.noreply.github.com>
2026-04-06 13:21:07 -0600

    Add: valid-word-square - submission-3

---
+++ Data Structures & Algorithms/valid-word-square/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        n = len(words)
+        for i in range(n):
+            for j in range(len(words[i])):
+                if j >= len(words) or i >= len(words[j]) or words[i][j] != words[j][i]:
+                    return False
+        return True
